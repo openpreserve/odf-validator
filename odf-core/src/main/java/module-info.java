284484a7424eae7ed9afcc7e8c7c7663d894fc30
Carl Wilson <carl@openpreservation.org>
2023-07-19 16:28:22 +0100

    REFACT: Modular JAR build
    - added `module-info.java` to both core and apps projects; and
    - removed JAR plugin config from `pom.xml`.

---
+++ odf-core/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.openpreservation.odf.core {
+    requires java.xml;
+
+    exports org.openpreservation.odf.fmt;
+    exports org.openpreservation.odf.pkg;
+    exports org.openpreservation.messages;
+    exports org.openpreservation.odf.validation;
+}
