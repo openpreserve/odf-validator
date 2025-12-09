eec41c5f2015a191c9ca1c7160bbe341a510bc76
Carl Wilson <carl@openpreservation.org>
2025-12-09 15:15:59 +0000

    MAINT: Fixed Codacy warnings
    - combined if statements; and
    - avoided parameter assignment.

--- odf-core/src/main/java/org/openpreservation/odf/validation/OdfXmlValidator.java
+++ odf-core/src/main/java/org/openpreservation/odf/validation/OdfXmlValidator.java
@@ -37,40 +37,40 @@ public class OdfXmlValidator {
 
     public static List<Message> getDocInfoMessages(final OdfXmlDocument doc) {
         List<Message> messages = new ArrayList<>();
-        if (doc.getParseResult().isWellFormed()) {
-            if (doc.getParseResult().isRootName("office:document")) {
-                messages.add(FACTORY.getInfo("DOC-2",
-                        Messages.parameterListInstance().add("version", doc.getVersion().version)));
-                if (doc.getFormat().isPackage()) {
-                    messages.add(FACTORY.getInfo("DOC-3",
-                            Messages.parameterListInstance().add("mimetype", doc.getFormat().mime)));
-                } else {
-                    messages.add(FACTORY.getError("DOC-4",
-                            Messages.parameterListInstance().add("mimetype", doc.getFormat().mime)));
-                }
+        if (doc.getParseResult().isWellFormed() && doc.getParseResult().isRootName("office:document")) {
+            messages.add(FACTORY.getInfo("DOC-2",
+                    Messages.parameterListInstance().add("version", doc.getVersion().version)));
+            if (doc.getFormat().isPackage()) {
+                messages.add(FACTORY.getInfo("DOC-3",
+                        Messages.parameterListInstance().add("mimetype", doc.getFormat().mime)));
+            } else {
+                messages.add(FACTORY.getError("DOC-4",
+                        Messages.parameterListInstance().add("mimetype", doc.getFormat().mime)));
             }
         }
         return messages;
     }
+
     public List<Message> validate(final Path toValidate, ParseResult parseResult) throws IOException {
-      List<Message> messages = new ArrayList<>();
-        if (parseResult.isWellFormed()) {
+        List<Message> messages = new ArrayList<>();
+        ParseResult result = parseResult;
+        if (result.isWellFormed()) {
             Version version = Version.ODF_13;
-            final OdfXmlDocument doc = OdfXmlDocuments.odfXmlDocumentOf(parseResult);
+            final OdfXmlDocument doc = OdfXmlDocuments.odfXmlDocumentOf(result);
             if (doc.isExtended()) {
                 ParameterList params = Messages.parameterListInstance().add("namespaces",
-                        Utils.collectNsPrefixes(OdfXmlDocuments.odfXmlDocumentOf(parseResult)
+                        Utils.collectNsPrefixes(OdfXmlDocuments.odfXmlDocumentOf(result)
                                 .getForeignNamespaces()));
                 messages.add(FACTORY.getMessage("DOC-8", this.isExtended ? Severity.INFO : Severity.ERROR, params));
             }
             if (this.isExtended || !doc.isExtended()) {
                 final Schema schema = new OdfSchemaFactory().getSchema(OdfNamespaces.OFFICE, version);
-                parseResult = validator.validate(parseResult, Files.newInputStream(toValidate), schema);
+                result = validator.validate(parseResult, Files.newInputStream(toValidate), schema);
             }
         } else {
             messages.add(FACTORY.getError("DOC-1"));
         }
-        messages.addAll(parseResult.getMessages());
+        messages.addAll(result.getMessages());
         return messages;
     }
 }
